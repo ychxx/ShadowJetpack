8777f1746e8a6676d694522235f5dc2fefc66724
sindicly <982438505@qq.com>
2021-09-29 16:46:16 +0800

    滚轮选择器优化

--- jetpackLib/src/main/java/com/yc/jetpacklib/widget/pickerview/YcPickerView.kt
+++ jetpackLib/src/main/java/com/yc/jetpacklib/widget/pickerview/YcPickerView.kt
@@ -1,13 +1,11 @@
 @file:Suppress("DUPLICATE_LABEL_IN_WHEN")
 
 package com.yc.jetpacklib.widget.pickerview
-
 import android.app.Activity
 import android.view.View
 import androidx.annotation.StringDef
 import com.bigkoo.pickerview.builder.OptionsPickerBuilder
 import com.bigkoo.pickerview.builder.TimePickerBuilder
-import com.bigkoo.pickerview.configure.PickerOptions
 import com.bigkoo.pickerview.view.OptionsPickerView
 import com.bigkoo.pickerview.view.TimePickerView
 import com.yc.jetpacklib.extension.hideSoftInput
@@ -31,42 +29,48 @@ object YcPickerView {
     @kotlin.annotation.Retention(AnnotationRetention.SOURCE)
     internal annotation class Type
 
+    var pvCondition: OptionsPickerView<String>? = null
+    var pvTime: TimePickerView? = null
 
     /*默认条件选择器*/
     fun showDefaultPickerWithCondition(
-        activity: Activity, titleText: String?,
-        pickerList: List<String>, selectCallBack: ((Int, Int, Int, View?) -> Unit)? = null
+        activity: Activity, titleText: String?, pickerList: List<String>,
+        selectCallBack: ((Int, Int, Int, View?) -> Unit)? = null
     ) {
-        val pv: OptionsPickerView<String> = OptionsPickerBuilder(activity) { options1: Int, options2: Int, options3: Int, v: View? ->
-            selectCallBack?.invoke(options1, options2, options3, v)
-        }.pickerDefaultStyle(activity).build()
-        pv.setTitleText(titleText)
-        pv.setPicker(pickerList) //二级选择器
+        if (pvCondition == null) {
+            pvCondition = OptionsPickerBuilder(activity) { options1: Int, options2: Int, options3: Int, v: View? ->
+                selectCallBack?.invoke(options1, options2, options3, v)
+            }.pickerDefaultStyle(activity).build()
+        }
+        pvCondition?.setTitleText(titleText)
+        pvCondition?.setPicker(pickerList) //二级选择器
         activity.hideSoftInput()
-        pv.show()
+        pvCondition?.show()
     }
 
 
     /*时间选择器*/
     fun showTimePicker(
-        activity: Activity, @Type type: String, titleText: String? = null,
-        selectedDefaultValue: Calendar? = null, selectCallBack: ((Date, View?) -> Unit)? = null
+        activity: Activity, @Type type: String, titleText: String? = null, selectedDefaultValue: Calendar? = null,
+        selectCallBack: ((Date, View?) -> Unit)? = null
     ) {
-        val pvTime = TimePickerBuilder(activity) { date, v ->
-            selectCallBack?.invoke(date, v)
-        }.apply {
-            when (type) {
-                YearMonthDate -> setType(booleanArrayOf(true, true, true, false, false, false))//只显示年月日
-                YearMonth -> setType(booleanArrayOf(true, true, false, false, false, false))//只显示年月
-                HourMinute -> setType(booleanArrayOf(false, false, false, true, true, false))//只显示时分
-                else -> setType(booleanArrayOf(true, true, true, true, true, true))//只显示年月
-            }
-        }.timePickerStyle(activity).build()
-        pvTime.setTimePickerDialogStyle()
-        pvTime.setTitleText(titleText)
-        if (selectedDefaultValue != null) pvTime.setDate(selectedDefaultValue)
+        if (pvTime == null) {
+            val pvTime = TimePickerBuilder(activity) { date, v ->
+                selectCallBack?.invoke(date, v)
+            }.apply {
+                when (type) {
+                    YearMonthDate -> setType(booleanArrayOf(true, true, true, false, false, false))//只显示年月日
+                    YearMonth -> setType(booleanArrayOf(true, true, false, false, false, false))//只显示年月
+                    HourMinute -> setType(booleanArrayOf(false, false, false, true, true, false))//只显示时分
+                    else -> setType(booleanArrayOf(true, true, true, true, true, true))//只显示年月
+                }
+            }.timePickerStyle(activity).build()
+        }
+        pvTime?.setTimePickerDialogStyle()
+        pvTime?.setTitleText(titleText)
+        if (selectedDefaultValue != null) pvTime?.setDate(selectedDefaultValue)
         activity.hideSoftInput()
-        pvTime.show()
+        pvTime?.show()
     }
 
 }
