8c432157bc498b3ac1a163a5fd7992f731d0627d
yc <563919364@qq.com>
2022-01-17 17:21:34 +0800

    1.仓库层添加 suspend转换flow

--- jetpackLib/src/main/java/com/yc/jetpacklib/base/YcRepository.kt
+++ jetpackLib/src/main/java/com/yc/jetpacklib/base/YcRepository.kt
@@ -33,4 +33,34 @@ open class YcRepository {
         failCall.invoke(cause.toYcException())
         throw cause
     }.checkNet()
+
+
+    fun <Data> ycToFlow(block: suspend () -> Data) = ycFlow<Data> {
+        send(YcResult.Success(block.invoke()))
+    }
+
+    fun <Data, T1> ycToFlow(t1: T1, block: suspend (T1) -> Data) = ycFlow<Data> {
+        send(YcResult.Success(block.invoke(t1)))
+    }
+
+    fun <Data, T1, T2> ycToFlow(t1: T1, t2: T2, block: suspend (T1, T2) -> Data) = ycFlow<Data> {
+        send(YcResult.Success(block.invoke(t1, t2)))
+    }
+
+    fun <Data, T1, T2, T3> ycToFlow(t1: T1, t2: T2, t3: T3, block: suspend (T1, T2, T3) -> Data) = ycFlow<Data> {
+        send(YcResult.Success(block.invoke(t1, t2, t3)))
+    }
+
+    fun <Data, T1, T2, T3, T4> ycToFlow(t1: T1, t2: T2, t3: T3, t4: T4, block: suspend (T1, T2, T3, T4) -> Data) = ycFlow<Data> {
+        send(YcResult.Success(block.invoke(t1, t2, t3, t4)))
+    }
+
+    fun <Data, T1, T2, T3, T4, T5> ycToFlow(t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, block: suspend (T1, T2, T3, T4, T5) -> Data) = ycFlow<Data> {
+        send(YcResult.Success(block.invoke(t1, t2, t3, t4, t5)))
+    }
+
+    fun <Data, T1, T2, T3, T4, T5, T6> ycToFlow(t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, block: suspend (T1, T2, T3, T4, T5, T6) -> Data) =
+        ycFlow<Data> {
+            send(YcResult.Success(block.invoke(t1, t2, t3, t4, t5, t6)))
+        }
 }
